5aca148d573da745fa43d798ed371a1a7db44e54
brindhakuma <160593693+brindhakuma@users.noreply.github.com>
2026-01-12 19:27:02 +0530

    hi

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pg#l+b#%z=)09g3@da9aewbz*fm5%e#$!yv)_2i6f1(06p_93c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['umaraaj.pythonanywhere.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
